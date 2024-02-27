74d4c631f2a3fa459868a4664cb63ef699656d0c
RameesMhd <muhammed.ramees@neopraxis.in>
2024-02-27 12:30:51 +0530

    New responsive Navbar.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Global Styles/GlobalStyles.scss';
 import MyNavbar from './components/Navbar/Navbar.js';
 
 function App() {
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,57 @@
+import './Navbar.scss';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const MyNavbar = () => {
     return (
         <>
-            <Navbar bg="light" data-bs-theme="light">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+            {['sm'].map((expand) => (
+                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+                    <Container>
+                        <Navbar.Brand href="#">
+                            <img src='' />
+                            Brand Logo
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Offcanvas
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="#home">Home</Nav.Link>
+                                    <Nav.Link href="#about">About Us</Nav.Link>
+                                    <NavDropdown
+                                        title="Programs"
+                                        id={`offcanvasNavbarDropdown-expand-${expand}`}
+                                    >
+                                        <NavDropdown.Item href="#program1">
+                                            Program 1
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="#program2">
+                                            Program 2
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="#program3">
+                                            Program 3
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                    <Nav.Link href="#about">Contact Us</Nav.Link>
+                                    <Nav.Link href="#gallery">Gallery</Nav.Link>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
         </>
     );
 }
