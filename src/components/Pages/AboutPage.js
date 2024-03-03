9d2bba6eb9a9a0598181be54853fb6bc3f417ca8
RameesMhd <muhammed.ramees@neopraxis.in>
2024-03-03 23:12:36 +0530

    NewPage added for About Us

---
+++ src/components/Pages/AboutPage.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const AboutPage = () =>{
+    return(
+        <div className='flex '>
+            About Page
+            <Link to="/">Go to Home</Link>
+        </div>
+    )
+}
+
+export default AboutPage
