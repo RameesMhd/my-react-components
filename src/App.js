f01aabda477455eb4a817dc351a0141ab08ae55d
RameesMhd <muhammed.ramees@neopraxis.in>
2024-03-03 23:13:08 +0530

    react router enabled for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Global Styles/GlobalStyles.scss';
-import MainPage from './components/MainPage.js';
+import HomePage from './components/HomePage.js';
+import AboutPage from './components/Pages/AboutPage.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <div>404 Error</div>
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  }
+]);
+
 function App() {
   return (
     <div className="App">
-      <MainPage />
+        <Layout>
+          <RouterProvider router={router} />
+        </Layout>
     </div>
   );
 }
